35b62f77f628da18b1cb0eb7224f8acfe2bfec87
Davide Paci <23656588+davidepaci@users.noreply.github.com>
2023-01-20 08:41:19 +0100

    refactor: separated js from page; only execute custom js after DOM load

--- frontend/js/input.js
+++ frontend/js/input.js
@@ -1,2 +1,15 @@
-// do AJAX post call to 127.0.0.1:8000/api/send
-// with the data from the form
+// After the DOM has loaded
+document.addEventListener("DOMContentLoaded", function(){
+    // Make a POST call with JSON data
+    document.getElementById("sendButton").addEventListener("click", function(){
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://127.0.0.1:3000/api/send", true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                console.alert("Conferma ricezione");
+            }
+        };
+        xhr.send(JSON.stringify([{id: 1, value: 10},{id: 2, value: 30}]));
+    });
+});
